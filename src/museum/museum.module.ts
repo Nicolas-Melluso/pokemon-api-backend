00e5a072ab8ac7c2fd292546dd640cdce5731eb0
Lucas Bianco <103225865+HebrineX@users.noreply.github.com>
2023-01-30 15:28:30 -0300

    Feature: ND-2 adding structure (#4)
    * Creating project in nestJS
    
    * Solving conflicts with previous pull request
    
    * add structure for pokemon,museum and users
    
    * solve conflicts and more

---
+++ src/museum/museum.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { MuseumController } from './museum.controller';
+import { MuseumService } from './museum.service';
+
+@Module({
+  controllers: [MuseumController],
+  providers: [MuseumService],
+})
+export class MuseumModule {}
