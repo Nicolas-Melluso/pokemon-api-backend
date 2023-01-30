cd1ed316b11a89669f1e272b6976a1c3d30805ab
Hebrinex <biancolucasgerman@gmail.com>
2023-01-30 14:41:00 -0300

    add structure for pokemon,museum and users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PokemonController } from './pokemon/pokemon.controller';
+import { PokemonService } from './pokemon/pokemon.service';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { MuseumModule } from './museum/museum.module';
+import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { MuseumController } from './museum/museum.controller';
+import { MuseumService } from './museum/museum.service';
+
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [PokemonModule, UserModule, MuseumModule],
+  controllers: [PokemonController, UserController, MuseumController],
+  providers: [PokemonService, UserService, MuseumService],
 })
 export class AppModule {}
